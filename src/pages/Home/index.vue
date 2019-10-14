bd8ffce033a346187bbc286725db194c0a974e90
liuhuihao <1061036802@qq.com>
2019-10-14 15:59:04 +0800

    feat: init

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <video id="img"></video>
+    <button>Start</button>
+  </div>
+</template>
+
+<script>
+  // import * as handTrack from 'handtrackjs'
+
+  export default {
+    name: 'index',
+    mounted () {
+
+    }
+  }
+</script>
+
+<style>
+  video {
+    width: 500px;
+    height: 500px;
+    border: 1px solid gray;
+  }
+</style>
